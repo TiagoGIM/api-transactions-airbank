0190eb6007598d1a6af250c302858f4ba3189631
Tiago <herique.sa@gmail.com>
2022-04-14 17:33:59 -0300

    created query to get all transactions

---
+++ README.md
@@ -0,0 +1,30 @@
+## Endpoints and Mocks
+- Heroku
+https://api-airbank-transactions.herokuapp.com/api
+
+- Postman
+https://7d19cadf-2b61-4342-9168-469e5ad104a2.mock.pstmn.io/query/transactions
+
+
+### Database
+Foi usado dois bancos postgres devido a limitaçao da conta gratuita e necessidade do prisma de ter um segundo banco para fazer migracoes.
+
+Env Variables
+DATABASE_URL = url main db
+SHADOW_DATABASE_URL = transiction migration db
+
+## Technologies
+- Express
+- ApolloServer
+- Prisma (OMR)
+- Postgress
+
+## References
+- To generate a sample database
+https://www.prisma.io/docs/guides/database/seed-database
+- SHADOW BASE to cloud db configs
+https://www.prisma.io/docs/concepts/components/prisma-migrate/shadow-database
+- Setup and deploy a graphql server with apolloserver
+https://tomray.dev/setup-and-deploy-graphql-server#configuring-typescript
+- create schema apollo
+https://www.apollographql.com/docs/tutorial/schema/
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { createServer } from "http";
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const startServer = async () => { 
 
@@ -8,18 +10,32 @@ const startServer = async () => {
   const httpServer = createServer(app)
 
   const typeDefs = gql`
-    type Query {
-      hello: String
-    }
-  `;
+  type Query {
+    transactions: [Transaction]
+  }
 
+  type Transaction {
+  id: String
+  account: String
+  description: String
+  category: String
+  reference: String
+  currency: String
+  amount: Float
+  status: String
+  transactionDate: String
+  createdAt: String
+  updatedAt: String
+}
+`;
 
   const resolvers = {
     Query: {
-      hello: () => 'First commit just config Apollo!',
+      transactions: () => {
+        return prisma.transaction.findMany()
+      },
     },
   };
-
 
   const apolloServer = new ApolloServer({
     typeDefs,
@@ -27,14 +43,13 @@ const startServer = async () => {
   })
 
   await apolloServer.start()
-
   apolloServer.applyMiddleware({
       app,
       path: '/api'
   })
-
+  let port = 3300;
   httpServer.listen({ port: process.env.PORT || 3300 }, () =>
-    console.log(`Server listening on localhost:4000${apolloServer.graphqlPath}`)
+    console.log(`Server listening on localhost:${port}${apolloServer.graphqlPath}`)
   )
 }
 
