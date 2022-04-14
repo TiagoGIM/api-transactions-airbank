278d8568093073736ab06d28232b6b45235ca656
Tiago <herique.sa@gmail.com>
2022-04-14 15:07:38 -0300

    Type of transaction

---
+++ src/types/schema.ts
@@ -0,0 +1,13 @@
+type Transactions = {
+  id: String
+  account: String
+  description: String
+  category: String
+  reference: String
+  currency: String
+  amount: number
+  status: String
+  transactionDate: String
+  createdAt: String
+  updatedAt: String
+}
